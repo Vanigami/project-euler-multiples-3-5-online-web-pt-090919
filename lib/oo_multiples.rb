003766ea0572af6b39e923ba5ce8666b08427575
Vanna Winland <vgwinland@gmail.com>
2020-10-22 22:08:41 +0000

    Automatically backed up by Learn

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -3,10 +3,11 @@ class Multiples
 
 def init(limit)
   @limit = limit
+  @multiples = collect_multiples
 end
 
 def collect_multiples
-  (1...limit).to_a.select({|num|num % 3 == 0 || num % 5 == 0})
+  (1...limit).to_a.select({|num| num % 3 == 0 || num % 5 == 0})
 end
 
 
