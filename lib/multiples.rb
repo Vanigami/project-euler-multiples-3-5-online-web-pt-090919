4af3b724e926f1666724c6be248789c377482909
Vanna Winland <vgwinland@gmail.com>
2020-10-22 21:59:44 +0000

    Automatically backed up by Learn

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -6,6 +6,6 @@ def collect_multiples(limit)
     (1...limit).to_a.select{|num| num % 3 == 0 || num % 5 == 0}
 end
 
-def sum_multiples()
-  collect_multiples.sum
+def sum_multiples(limit)
+  collect_multiples(limit).sum
 end
