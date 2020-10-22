84b3b50e88d863c4dec5c9c0c7fe7d3d1bdf47d1
Vanna Winland <vgwinland@gmail.com>
2020-10-22 21:44:59 +0000

    Automatically backed up by Learn

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -2,5 +2,5 @@
 # If we list all of the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6, and 9. The sum of these multiples is 23.
 # Find the __sum__ of all the multiples of 3 or 5 below 1000.
 
-sum_multiples(3,5)
+def sum_multiples(3,5)
 end
